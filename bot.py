415fde73effb72df557d29798119174b0a6b00b6
fattilatshirt <76770141+fattilatshirt@users.noreply.github.com>
2025-06-25 13:57:57 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -725,7 +725,7 @@ async def command_error(ctx, error):
         await ctx.send(f'❌ Si è verificato un errore: {error}')
 
 def run_web():
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=port)
 
 TOKEN = os.environ["TOKEN"]
